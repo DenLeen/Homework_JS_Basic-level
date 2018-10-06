3bce5465b184c837d6c1b85663bc547a7217dabb
Denis Lindorenko <Dlindorenko@tut.by>
2018-10-06 22:00:36 +0300

    Lesson_6

--- Lesson_06/chess/main.js
+++ Lesson_06/chess/main.js
@@ -6,15 +6,15 @@ createBtn.onclick = function clickBtn() {
     chessField.appendChild(tBody);
 
     var x = document.getElementById('fieldX').value;
-        if (x <1 || x>10) {
-            alert ('Ошибка');
-            createBtn.style.display = none;
-        }
+    if (x < 1 || x > 10) {
+        alert('Ошибка');
+        createBtn.style.display = none;
+    }
     var y = document.getElementById('fieldY').value;
-        if (y <1 || y>10) {
-            alert ('Ошибка');
-            createBtn.style.display = none;
-        }
+    if (y < 1 || y > 10) {
+        alert('Ошибка');
+        createBtn.style.display = none;
+    }
     for (var i = 0; i < x; i++) {
         var tr = document.createElement('TR');
         tBody.appendChild(tr);
@@ -23,8 +23,22 @@ createBtn.onclick = function clickBtn() {
             tr.appendChild(td);
         }
     }
-    createBtn.onclick = function() {
+
+    createBtn.onclick = function () {
         createBtn.style.display = none;
+    }
+    tr[a].addEventListener('click',colorField(),false);
+    td[b].addEventListener('click',colorField(),false);
+
+    function colorField() {
+    var x = document.getElementById('fieldX').value,
+        y = document.getElementById('fieldY').value;
+    for (var a = 1; a < y; a+2) {
+        document.getElementsByTagName('tr')[a].toggle('dark');
+    }
+    for (var b = 1 ; b < x; b+2 ){
+        document.getElementsByTagName('td')[b].toggle('dark');
+        }
     }
 }
 
