515954364a7dc612b1f4b08c20a37c3e0f41bcdf
Denis Lindorenko <Dlindorenko@tut.by>
2018-10-09 18:49:48 +0300

    Less-6

--- Lesson_06/chess/main.js
+++ Lesson_06/chess/main.js
@@ -2,18 +2,19 @@ var createBtn = document.getElementById('create'),
     chessField = document.getElementsByTagName('table')[0];
 
 createBtn.onclick = function clickBtn() {
+    createBtn.setAttribute('disabled', true);
     var tBody = document.createElement('tbody');
     chessField.appendChild(tBody);
 
     var x = document.getElementById('fieldX').value;
     if (x < 1 || x > 10) {
         alert('Ошибка');
-        createBtn.style.display = none;
+        createBtn.setAttribute('disabled', true);
     }
     var y = document.getElementById('fieldY').value;
     if (y < 1 || y > 10) {
         alert('Ошибка');
-        createBtn.style.display = none;
+        createBtn.setAttribute('disabled', true);
     }
     for (var i = 0; i < x; i++) {
         var tr = document.createElement('TR');
@@ -21,25 +22,20 @@ createBtn.onclick = function clickBtn() {
         for (var j = 0; j < y; j++) {
             var td = document.createElement('TD');
             tr.appendChild(td);
+            if (i%2 == j%2) {
+                td.className = 'black';
+            }
+            // else {
+            //     td.className = 'black';
+            // }
         }
     }
+}
 
-    createBtn.onclick = function () {
-        createBtn.style.display = none;
-    }
-    tr[a].addEventListener('click',colorField(),false);
-    td[b].addEventListener('click',colorField(),false);
+var td = document.createElement('TD');
 
-    function colorField() {
-    var x = document.getElementById('fieldX').value,
-        y = document.getElementById('fieldY').value;
-    for (var a = 1; a < y; a+2) {
-        document.getElementsByTagName('tr')[a].toggle('dark');
-    }
-    for (var b = 1 ; b < x; b+2 ){
-        document.getElementsByTagName('td')[b].toggle('dark');
-        }
-    }
+chessField.onclick = function clickChessField() {
+    td.classList.toggle(black);
 }
 
 
